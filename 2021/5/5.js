9b09334b8534cfaa399cc11144400bcdc6cac2d3
jerryzhch <jerry_agboola@hotmail.com>
2021-12-05 19:47:34 +0100

    dont know why not working

--- 2021/5/5.js
+++ 2021/5/5.js
@@ -1,7 +1,8 @@
 var standard_input = process.stdin;
 // Set input character encoding.
 let dataArr = [];
-let map = createAndFillTwoDArray({ rows: 1000, columns: 1000, defaultValue: 0 });
+let map1 = createAndFillTwoDArray({ rows: 1000, columns: 1000, defaultValue: 0 });
+let map2 = createAndFillTwoDArray({ rows: 1000, columns: 1000, defaultValue: 0 });
 standard_input.setEncoding('utf-8');
 // Prompt user to input data in console.
 console.log('Please input text in command line.');
@@ -11,38 +12,54 @@ standard_input.on('data', function (data) {
   if (data.includes('exit')) {
     for (let i = 0; i < dataArr.length; i++) {
       const pair1 = dataArr[i][0].split(',');
-      const pair2 = dataArr[i][1].split(',');
-      if (pair1[0] == pair2[0] && pair1[1] == pair2[1] - 0) {
-        const startIndex = pair1[1] > pair2[1] - 0 ? pair2[1] - 0 : pair1[1];
-        const endIndex = pair1[1] < pair2[1] - 0 ? pair2[1] - 0 : pair1[1];
-        for (let j = startIndex; j < endIndex; j++) {
-          map[j][j] += 1;
+      let pair2 = dataArr[i][1].split(',');
+      pair2[1] -= 0;
+      if (pair1[0] == pair1[1] && pair2[0] == pair2[1]) {
+        const startIndex = pair1[1] > pair2[1] ? pair2[1] : pair1[1];
+        const endIndex = pair1[1] < pair2[1] ? pair2[1] : pair1[1];
+        for (let j = startIndex; j <= endIndex; j++) {
+          map2[j][j] = map2[j][j] + 1;
+        }
+      } else if (pair1[0] == pair2[1] && pair2[0] == pair1[1]) {
+        const startIndex = pair1[0];
+        let revStartIndex = pair1[1];
+        let revEndIndex = pair1[0];
+        while (!(revStartIndex == startIndex)) {
+          console.log(revStartIndex);
+          map2[revStartIndex][revEndIndex] = map2[revStartIndex][revEndIndex] + 1;
+          revStartIndex++;
+          revEndIndex--;
         }
       } else if (pair1[0] == pair2[0]) {
-        const startIndex = pair1[1] > pair2[1] - 0 ? pair2[1] - 0 : pair1[1];
-        const endIndex = pair1[1] < pair2[1] - 0 ? pair2[1] - 0 : pair1[1];
-        for (let j = startIndex; j < endIndex; j++) {
-          map[j][pair1[0]] += 1;
-          console.log(map[j][pair1[0]]);
+        const startIndex = pair1[1] > pair2[1] ? pair2[1] : pair1[1];
+        const endIndex = pair1[1] < pair2[1] ? pair2[1] : pair1[1];
+        for (let j = startIndex; j <= endIndex; j++) {
+          map1[pair1[0]][j] = map1[pair1[0]][j] + 1;
+          map2[pair1[0]][j] = map2[pair1[0]][j] + 1;
         }
-      } else if (pair1[1] == pair2[1] - 0) {
+      } else if (pair1[1] == pair2[1]) {
         const startIndex = pair1[0] > pair2[0] ? pair2[0] : pair1[0];
         const endIndex = pair1[0] < pair2[0] ? pair2[0] : pair1[0];
-        for (let j = startIndex; j < endIndex; j++) {
-          map[pair1[1]][j] += 1;
-          console.log(map[pair1[1]][j]);
+        for (let j = startIndex; j <= endIndex; j++) {
+          map1[j][pair1[1]] = map1[j][pair1[1]] + 1;
+          map2[j][pair1[1]] = map2[j][pair1[1]] + 1;
         }
       }
     }
     let counter = 0;
-    map.map((row) =>
-      row.map((el) => {
-        if (el >= 2) {
-          counter++;
-        }
-      })
-    );
-    console.log(counter);
+    map1.forEach((row) => {
+      row.forEach((el) => {
+        if (el > 1) counter++;
+      });
+    });
+    console.log('Part 1: ' + counter);
+    counter = 0;
+    map2.forEach((row) => {
+      row.forEach((el) => {
+        if (el - 0 > 1) counter++;
+      });
+    });
+    console.log('Part 2: ' + counter);
     process.exit();
   } else {
     const pair = data.split(' -> ');
