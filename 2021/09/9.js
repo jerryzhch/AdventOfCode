59934ccb84cc6d4e244dbf15bfcdabba7317f49c
jerryzhch <agboojer@students.zhaw.ch>
2021-12-09 09:09:03 +0100

    Update 9.js

--- 2021/09/9.js
+++ 2021/09/9.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+const fs = require("fs");
 
 let dataArr = [];
-fs.readFile('input2.txt', (err, data) => {
+fs.readFile("input.txt", (err, data) => {
   if (err) throw err;
 
   let dataR = [];
-  dataR = data.toString().split('\r\n');
+  dataR = data.toString().split("\r\n");
 
   dataR.forEach((el) => {
     let res = [];
@@ -14,84 +14,39 @@ fs.readFile('input2.txt', (err, data) => {
     }
     dataArr.push(res);
   });
-  let lowest = [];
+  let lowest = 0;
 
   for (let i = 0; i < dataArr.length; i++) {
     for (let j = 0; j < dataArr[i].length; j++) {
-      let current = dataArr[i][j];
-      if (i == 0) {
-        let right = dataArr[i][j + 1];
-        let down = dataArr[i + 1][j];
-        if (j == 0) {
-          if (current <= right && current <= down) {
-            lowest.push(current);
-            dataArr[i][j + 1] = 10;
-            dataArr[i + 1][j] = 10;
-          }
-        } else if (j == dataArr[i].length - 1) {
-          let left = dataArr[i][j - 1];
-          let down = dataArr[i + 1][j];
-          if (current <= left && current <= down) {
-            lowest.push(current);
-            dataArr[i][j - 1] = 10;
-            dataArr[i + 1][j] = 10;
-          }
-        } else {
-          let left = dataArr[i][j - 1];
-          if (current <= right && current <= down && current <= left) {
-            lowest.push(current);
-            dataArr[i][j + 1] = 10;
-            dataArr[i + 1][j] = 10;
-            dataArr[i][j - 1] = 10;
-          }
-        }
-        continue;
-      } else if (i == dataArr.length - 1) {
-        if (j == 0) {
-          let right = dataArr[i][j + 1];
-          let up = dataArr[i - 1][j];
-          if (current <= right && current <= up) {
-            lowest.push(current);
-            dataArr[i][j + 1] = 10;
-            dataArr[i - 1][j] = 10;
-          }
-        } else if (j == dataArr[i].length - 1) {
-          let left = dataArr[i][j - 1];
-          let up = dataArr[i - 1][j];
-          if (current <= left && current <= up) {
-            lowest.push(current);
-            dataArr[i][j - 1] = 10;
-            dataArr[i - 1][j] = 10;
-          }
-        } else {
-          let right = dataArr[i][j + 1];
-          let left = dataArr[i][j - 1];
-          let up = dataArr[i - 1][j];
-          if (current <= right && current <= up && current <= left) {
-            lowest.push(current);
-            dataArr[i][j + 1] = 10;
-            dataArr[i - 1][j] = 10;
-            dataArr[i][j - 1] = 10;
-          }
-        }
-        continue;
-      } else {
-        let right = dataArr[i][j + 1];
-        let up = dataArr[i - 1][j];
-        let down = dataArr[i + 1][j];
-        let left = dataArr[i][j - 1];
-        if (current <= right && current <= up && current <= left && current <= down) {
-          lowest.push(current);
-          dataArr[i][j + 1] = 10;
-          dataArr[i + 1][j] = 10;
-          dataArr[i][j - 1] = 10;
-          dataArr[i + 1][j] = 10;
-        }
+      if (isLowAdjacent(i, j)) {
+        lowest += dataArr[i][j] + 1;
       }
-      continue;
     }
   }
-  const result = lowest.map((el) => el + 1).reduce((a, b) => a + b);
-
-  console.log(result);
+  console.log(lowest);
 });
+function isLowAdjacent(i, j) {
+  let point = dataArr[i][j];
+  let n = -1;
+  let s = -1;
+  let e = -1;
+  let w = -1;
+  try {
+    n = dataArr[i - 1][j];
+  } catch (error) {}
+  try {
+    s = dataArr[i + 1][j];
+  } catch (error) {}
+  try {
+    e = dataArr[i][j + 1];
+  } catch (error) {}
+  try {
+    w = dataArr[i][j - 1];
+  } catch (error) {}
+  if (n >= 0 && point >= n) return false;
+  if (s >= 0 && point >= s) return false;
+  if (e >= 0 && point >= e) return false;
+  if (w >= 0 && point >= w) return false;
+
+  return true;
+}
